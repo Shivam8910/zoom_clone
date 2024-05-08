fe5a2e783de282d2bb1e143ed9cbb2d2ee28de83
Shivam Singh <ss8258561@gmail.com>
2024-05-08 17:19:56 +0530

    second commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,8 @@
-import StreamVideoProvider from "@/providers/StreamClientProvider";
-import { Metadata } from "next";
 import React, { ReactNode } from "react";
 
-export const metadata: Metadata = {
-  title: "YOOM",
-  description: "Video Calling App",
-  icons: {
-    icon: "/icons/logo.svg",
-  },
-};
+import StreamVideoProvider from "@/providers/StreamClientProvider";
 
+console.log(typeof StreamVideoProvider);
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
